504be97f06ce396556b92f0a744ecd20c20fe38d
Vitaly Takmazov <vitalyster@gmail.com>
2014-03-06 05:46:31 +0000

    Fix sending messages to GCM by switching to JSON

--- mod_smacks_notifications.lua
+++ mod_smacks_notifications.lua
@@ -2,6 +2,8 @@ local st = require "util.stanza";
 local xmlns_notifications_gcm = "http://sawim.ru/notifications#gcm";
 
 local http = require "net.http";
+local json_encode = require"util.json".encode;
+
 local gcm_uri = "https://android.googleapis.com/gcm/send";
 local GCM_API_KEY = module:get_option("gcm_api_key", nil);
 
@@ -25,7 +27,7 @@ function handle_notifications_command(event)
         elseif stanza.tags[1].name == "unregister" then
 		-- remove device
                 session.regid = nil
-		module:log("debug", "GCM device unregistered from session %s", session.resumption_token);
+		module:log("debug", "GCM regid unregistered from session %s", session.resumption_token);
 		session.send(st.reply(stanza));
                 return true;
         end
@@ -38,12 +40,26 @@ function handle_smacks_message(event)
         local session, stanza = event.origin, event.stanza;
         module:log("debug", "message to session %s", session.resumption_token);
 	if session.regid ~= nil then
-		-- send message to GCM
 		if GCM_API_KEY != nil then
+			-- send message to GCM
 			message_body = http.urlencode(stanza:get_child("body"):get_text());
-			http.request(gcm_uri, {headers = {name = "Authorization", value = GCM_API_KEY}, body = http.formencode({name = "data.message", value = message_body, name = "recipients_ids", value = session.regid, name = "collapse_key", value ="New messages"})}, function(response, code, request) 
-												module:log("debug", "GCM status %d", code)
-												end);
+			json_body = json_encode {
+                        	        collapse_key = "messages",
+                                	registration_ids = {session.regid},
+	                                data = { message = message_body, message_from = stanza.attr.from };
+        	                        };
+               		module:log("debug", json_body);
+			http.request(gcm_uri, {
+				headers = {
+					["Authorization"] = "key="..GCM_API_KEY;
+					["Content-Type"] = "application/json";
+				};
+				body = json_body;
+				}, function (resp, code, request)
+					module:log("debug", "GCM status %d", code);
+					tostring(resp);
+				    end
+				);
 		else
 			module:log("error", "GCM API key not defined");
 		end
